60e64a0c65c378929355218f2727f01b8f22be85
BrutPitt <brutpitt@gmail.com>
2019-07-25 03:38:53 +0200

    Added helper function getPosition(), it returns vec3(pan.x, pan.y, dolly.z)

--- imGuIZMO.example/src/tools/transforms.h
+++ imGuIZMO.example/src/tools/transforms.h
@@ -33,14 +33,15 @@ using namespace glm;
 
 struct transfMatrix {
     glm::mat4 vMatrix   = glm::mat4(1.0f);
-    glm::mat4 mMatrix   = glm::mat4(1.0f);      // Store the model matrix
-    glm::mat4 pMatrix   = glm::mat4(1.0f);
+    glm::mat4 mMatrix   = glm::mat4(1.0f);
+    glm::mat4 pMatrix   = glm::mat4(1.0f);      // Uniforms starts HERE!
     glm::mat4 mvMatrix  = glm::mat4(1.0f);
     glm::mat4 mvpMatrix = glm::mat4(1.0f);
+    glm::mat4 mvLightM  = glm::mat4(1.0f);
 };
 
 class transformsClass {
-#define SZ (sizeof(glm::mat4)*3)
+#define SZ (sizeof(glm::mat4)*4)
 public:
     transformsClass() {
         //setView(attractorsList.get()->getPOV(), attractorsList.get()->getTGT());         
@@ -121,18 +122,29 @@ public:
         tM.mvpMatrix = tM.pMatrix * tM.mvMatrix;
     }
 
-    void setView() {         
-        tM.vMatrix = glm::lookAt( povVec ,
+    void setView() {
+        tM.vMatrix = glm::lookAt( povVec,
                                   tgtVec,
                                   vec3(0.0f, 1.0f, 0.0f));
     }
 
     void setView(const vec3 &pov, const vec3 &tgt) {
         povVec = pov; tgtVec = tgt;
-        tM.vMatrix = glm::lookAt( povVec ,
+        tM.vMatrix = glm::lookAt( povVec,
                                   tgtVec,
                                   vec3(0.0f, 1.0f, 0.0f));
     }
+    void setLightView(vec3 &lightPos) {
+        //mat4 m(1.f);
+        //m = translate(m,getPOV());
+        //m = translate(m,getTrackball().getPosition());
+        tM.mvLightM = glm::lookAt(lightPos * .25f +povVec,
+                                  tgtVec, //getTrackball().getRotationCenter(),
+                                  vec3(0.0f, 1.0f, 0.0f));
+        //tM.mvLightM = tM.mvLightM * m;
+        //tM.mvLightM =  tM.mMatrix * tM.mvLightM ;
+    }
+
 #define MIN_NEAR .01 //(_far*.01f)
     void setPerspective(float angle, float aspect, float _near, float _far) {        
         pAngle = angle; pAspect = aspect; pNear = _near <= MIN_NEAR ? MIN_NEAR : _near; pFar = _near>_far ? _near : _far;
@@ -144,6 +156,10 @@ public:
         //tM.pMatrix  = glm::perspective(glm::radians(pAngle),pAspect,pNear, pFar); //the projection matrix
         tM.pMatrix  = glm::perspectiveFov(glm::radians(pAngle),float(theApp->GetWidth()), float(theApp->GetHeight()),pNear, pFar); //the projection matrix
     }
+    void setPerspective() {
+        //tM.pMatrix  = glm::perspective(glm::radians(pAngle),pAspect,pNear, pFar); //the projection matrix
+        tM.pMatrix  = glm::perspectiveFov(glm::radians(pAngle),float(theApp->GetWidth()), float(theApp->GetHeight()),pNear, pFar); //the projection matrix
+    }
     void setPerspective(float angle, float _near, float _far) {
         pAngle = angle; pNear = _near <= MIN_NEAR ? MIN_NEAR : _near; pFar = _near>_far ? _near : _far;
         //tM.pMatrix  = glm::perspective(glm::radians(pAngle),pAspect,pNear, pFar); //the projection matrix
@@ -151,6 +167,9 @@ public:
         
     }
 
+//    void setLightOrtho() {
+//        tM.pLightM = glm::ortho(-20.0f,20.0f,-20.0f,20.0f,pNear,pFar);
+//    }
     float getPerspAngleRad() { return glm::radians(pAngle); }
     float getPerspAngle() { return pAngle; }
     float getPerspNear() { return pNear; }
