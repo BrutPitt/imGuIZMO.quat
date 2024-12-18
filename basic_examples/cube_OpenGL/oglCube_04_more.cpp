e653e14017e6f420a3d484a011309f50f08a4e88
BrutPitt <brutpitt@gmail.com>
2024-12-18 06:48:44 +0100

    Basic OpenGL examples: how to implement imguizmo_quat widgets in a easy "cube" example

---
+++ basic_examples/cube_OpenGL/oglCube_04_more.cpp
@@ -0,0 +1,300 @@
+//------------------------------------------------------------------------------
+//  Copyright (c) 2018-2025 Michele Morrone
+//  All rights reserved.
+//
+//  https://michelemorrone.eu - https://brutpitt.com
+//
+//  X: https://x.com/BrutPitt - GitHub: https://github.com/BrutPitt
+//
+//  direct mail: brutpitt(at)gmail.com - me(at)michelemorrone.eu
+//
+//  This software is distributed under the terms of the BSD 2-Clause license
+//------------------------------------------------------------------------------
+#include <stdlib.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <GLFW/glfw3.h>
+
+#include "oglDebug.h"
+#include "shadersAndModel.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// imGuIZMO: include imGuIZMOquat.h or imguizmo_quat.h
+#include <imguizmo_quat.h> // now also imguizmo_quat.h from v3.1
+
+int width = 1280, height = 800;
+GLFWwindow *glfwWindow;
+
+const int nVertex = sizeof(coloredCubeData)/(sizeof(float)*2);
+GLuint nElemVtx = 4;
+
+// Shaders & Vertex attributes
+GLuint program, vao, vaoBuffer;
+
+mat4 mvpMatrix, viewMatrix, projMatrix, lightMatrix;
+vec3 light(2.5, 2.5, 2.5);
+enum loc { vtxIdx = 0, colIdx, mvpIdx, lightIdx};     // shader locations
+
+void draw()
+{
+    glUseProgram(program);
+
+    glProgramUniformMatrix4fv(program, loc::mvpIdx,   1, false, value_ptr(mvpMatrix)  );  // using vgMath instead of GLM you can cast to mat4*
+    glProgramUniformMatrix4fv(program, loc::lightIdx, 1, false, value_ptr(lightMatrix));  // vgMath also cast: (mat4*) or static_cast<mat4*>()
+
+    glBindVertexArray(vao);
+    //glDrawArrays(GL_TRIANGLES, 0, nVertex);
+    glDrawArraysInstanced(GL_TRIANGLES, 0, nVertex, 2);
+
+    glUseProgram(0);
+}
+
+void setCamera()
+{
+    float aspectRatio = float(height) / float(width);       // Set "camera" position and perspective
+    float fov = radians( 75.0f ) * aspectRatio;
+    vec3 upVec(0.0f, 1.0f, .0f);
+    viewMatrix = lookAt( vec3( 0.0f, 0.0f, 10.0f ),  vec3( 0.0f, 0.0f, 0.0f ),  upVec);
+    projMatrix = perspective( fov, 1/aspectRatio, 0.1f, 100.0f );
+}
+
+void glfwWindowSizeCallback(GLFWwindow* window, int w, int h)
+{
+    width = w; height = h;
+    setCamera();
+    glViewport(0, 0, width, height);
+    draw();
+}
+
+void initGL()
+{
+    enableDebugCallback();
+
+    GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vertex_instanced, NULL);
+    glCompileShader(vertex);
+    glCompileShader(vertex);
+
+    GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fragment_code, NULL);
+    glCompileShader(fragment);
+    checkShader(fragment);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
+    checkProgram(program);
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    glCreateVertexArrays(1, &vao);
+    glCreateBuffers(1, &vaoBuffer);
+    glNamedBufferStorage(vaoBuffer, sizeof(coloredCubeData), coloredCubeData, 0);
+
+    glVertexArrayAttribBinding(vao,loc::vtxIdx, 0);
+    glVertexArrayAttribFormat(vao, loc::vtxIdx, nElemVtx, GL_FLOAT, GL_FALSE, 0);
+    glEnableVertexArrayAttrib(vao, loc::vtxIdx);
+
+    glVertexArrayAttribBinding(vao,loc::colIdx, 0);
+    glVertexArrayAttribFormat(vao, loc::colIdx, nElemVtx, GL_FLOAT, GL_FALSE, nElemVtx*sizeof(float));
+    glEnableVertexArrayAttrib(vao, loc::colIdx);
+
+    glVertexArrayVertexBuffer(vao, 0, vaoBuffer, 0, 2*nElemVtx*sizeof(float));
+
+    glViewport(0, 0, width, height);
+
+    glDisable(GL_BLEND);
+    glDepthMask(GL_TRUE);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glFrontFace(GL_CW);
+
+    glDepthRange(-1.0, 1.0);
+    setCamera();
+}
+
+void initGLFW()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
+
+    glfwWindow = glfwCreateWindow(width, height, "glCube", NULL, NULL);
+    if (!glfwWindow)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(glfwWindow);
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);  //get OpenGL extensions
+
+    glfwSetWindowSizeCallback(glfwWindow, glfwWindowSizeCallback);
+
+    glfwSwapInterval(1); // 0 vSync off - 1 vSync on
+}
+
+void initImGui()
+{
+        // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(glfwWindow, true);
+    ImGui_ImplOpenGL3_Init("#version 450");
+}
+
+
+int main()
+{
+    initGLFW();         // initialize GLFW framework
+    initGL();           // init OpenGL building vaoBuffer and shader program (compile and link vtx/frag shaders)
+
+    // other OpenGL settings... used locally
+    vec4 bgColor = vec4(0.0f);
+    GLfloat f=1.0f;
+
+    // Setup/Initialize Dear ImGui context
+    initImGui();
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    // imGuIZMO: set mouse feeling and mods
+    ///////////////////////////////////
+    imguiGizmo::setGizmoFeelingRot(2.f);                    // default 1.0, >1 more mouse sensitivity, <1 less mouse sensitivity
+    imguiGizmo::setPanScale(3.5f);                          // default 1.0, >1 more, <1 less
+    imguiGizmo::setDollyScale(3.5f);                        // default 1.0, >1 more, <1 less
+    imguiGizmo::setDollyWheelScale(7.0f);                   // default 2.0, > more, < less ... (from v3.1 separate values)
+    imguiGizmo::setPanModifier(vg::evSuperModifier);        // change KEY modifier: CTRL (default) ==> SUPER
+    imguiGizmo::setDollyModifier(vg::evControlModifier);    // change KEY modifier: SHIFT (default) ==> CTRL
+
+    // main render/draw loop
+    while (!glfwWindowShouldClose(glfwWindow)) {
+        glfwPollEvents();
+        glClearBufferfv(GL_DEPTH, 0, &f);
+        glClearBufferfv(GL_COLOR, 0, value_ptr(bgColor));
+
+        if (glfwGetWindowAttrib(glfwWindow, GLFW_ICONIFIED) != 0)   {
+            ImGui_ImplGlfw_Sleep(10);
+            continue;
+        }
+
+    // ImGUI: prepare new frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+    // ImGui: Your windows here
+
+    // Imgui window: build a transparent window (container) to insert widget
+        float widgetSize=240;
+        ImGui::SetNextWindowSize(ImVec2(widgetSize, height), ImGuiCond_Always); // top ...
+        ImGui::SetNextWindowPos(ImVec2(width-widgetSize, 0), ImGuiCond_Always); // ... right aligned
+
+        ImGui::PushStyleColor(ImGuiCol_WindowBg,ImVec4(0.f,0.f,0.f,0.f));       // transparent Wnd Background
+        ImGui::PushStyleColor(ImGuiCol_FrameBg,ImVec4(0.f,0.f,0.f,0.f));        // transparent frame Background
+        const float prevWindowBorderSize = style.WindowBorderSize;              // Save current border size...
+        style.WindowBorderSize = .0f;                                           // ... to draw the window with ZERO broder
+
+        bool isVisible = true;                                                  // visibility flag: always on
+        ImGui::Begin("##giz", &isVisible, ImGuiWindowFlags_NoTitleBar|          // noTitle / noResize / Noscrollbar
+                                          ImGuiWindowFlags_NoResize|
+                                          ImGuiWindowFlags_NoScrollbar);
+
+    // imGuIZMO: declare global/static/member/..
+    ///////////////////////////////////
+        static quat rotation(1,0,0,0);       // quat default constructor initialize @ quat(1,0,0,0) ==> w(1) x(0) y(0) z(0), w is left/first value
+        static vec3 position;                // default initialization vec3(0)
+        vec3 tmpLight = -light;              // Light Vector have inverse direction (toward origin) so using a tmp vect
+
+    // colored text for display quat(w,x,y,z) components
+        ImGui::SetCursorPos(ImVec2(0,0));
+        ImGui::PushItemWidth(widgetSize*.25-2);
+        ImGui::TextColored(ImVec4(1,1,1,1), "w: % 1.2f", rotation.w); ImGui::SameLine();
+        ImGui::TextColored(ImVec4(1,0,0,1), "x: % 1.2f", rotation.x); ImGui::SameLine();
+        ImGui::TextColored(ImVec4(0,1,0,1), "y: % 1.2f", rotation.y); ImGui::SameLine();
+        ImGui::TextColored(ImVec4(0,0,1,1), "z: % 1.2f", rotation.z);
+        ImGui::PopItemWidth();
+
+    // ImGuIZMO.quat widget
+    ///////////////////////////////////
+        if(ImGui::gizmo3D("##aaa", rotation, tmpLight, widgetSize)) // if(ImGui::gizmo3D(...) == true) ---> widget has been updated
+            light = -tmpLight;                                      // restore sign from acquired rotation
+
+    // ImGuIZMO.quat with also pan and Dolly/zoom ... and Sphere @ origin
+    ///////////////////////////////////
+        ImGui::gizmo3D("##a01", position, rotation, widgetSize/2, imguiGizmo::sphereAtOrigin);    // Ctrl+LButton = Pan ... Shift+LButton = Dolly/Zoom
+        ImGui::SameLine();
+    // ImGuIZMO.quat same previous one, but changing central solid and axes length
+    ///////////////////////////////////
+        imguiGizmo::resizeAxesOf(vec3(.5, 1, 1));  // axes len, thickness, thickness
+        imguiGizmo::resizeSolidOf(1.5);             // central solid size (1.0 default): 1.2 sphere bigger
+        ImGui::gizmo3D("##a11", position, rotation, widgetSize/2, imguiGizmo::sphereAtOrigin);    // Ctrl+LButton = Pan ... Shift+LButton = Dolly/Zoom
+                                                    // until not restoring sizes all widgets are draw with this sizes
+        imguiGizmo::restoreSolidSize();             // restore at default otherwise other widgets maintain this settings
+        imguiGizmo::restoreAxesSize();              // restore at default
+
+    // ImGuIZMO.quat with also pan and Dolly/zoom
+    ///////////////////////////////////
+        if(ImGui::gizmo3D("##a02", tmpLight, widgetSize))           // Directional Widget
+            light = -tmpLight;                                      // restore sign from acquired rotation
+
+    // End Imgui window (container) block
+        ImGui::End();
+        style.WindowBorderSize = prevWindowBorderSize;              // restore border size
+        ImGui::PopStyleColor();                                     // frame color (pushed)
+        ImGui::PopStyleColor();                                     // Background (pushed)
+
+    // now we can transfer the rotation in a matrix... with alternative modes: all new from v3.1 except math_cast()
+        mat4 modelMatrix(rotation);                                 // constructor
+        modelMatrix = mat4_cast(rotation);                          // existing matrix assignation
+        modelMatrix = mat4(rotation);                               //    "        "       "
+
+    // Build a "translation" matrix
+        mat4 translationMatrix = translate(mat4(1), position);      // add translations (pan/dolly) to an identity matrix
+    // build MVP matrix to pass to shader
+        mvpMatrix   = projMatrix * translationMatrix * viewMatrix * static_cast<mat4>(rotation);
+        lightMatrix = projMatrix * translationMatrix * translate(mat4(1), light) * viewMatrix;
+
+    // draw the cube, passing MVP matrix to the vtx shader
+        draw();
+
+    // ImGui Rendering
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(glfwWindow);
+    }
+
+    // Cleanup ImGui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    // Cleanup OpenGL
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vaoBuffer);
+    glDeleteProgram(program);
+
+    // Cleanup GLFW
+    glfwDestroyWindow(glfwWindow);
+    glfwTerminate();
+}
