3fbe05271ec76a718caa5e587ac51b03c45443e4
BrutPitt <brutpitt@gmail.com>
2024-12-19 06:50:58 +0100

    Basic OpenGL examples: how to implement imguizmo_quat widgets in a easy "cube" example

---
+++ basic_examples/cube_OpenGL/oglDebug.h
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------
+//  Copyright (c) 2018-2024 Michele Morrone
+//  All rights reserved.
+//
+//  https://michelemorrone.eu - https://BrutPitt.com
+//
+//  twitter: https://twitter.com/BrutPitt - github: https://github.com/BrutPitt
+//
+//  mailto:brutpitt@gmail.com - mailto:me@michelemorrone.eu
+//  
+//  This software is distributed under the terms of the BSD 2-Clause license
+//------------------------------------------------------------------------------
+#pragma once
+void getCompilerLog(GLuint handle, GLint blen, bool isShader);
+void checkShader(GLuint shader);
+void checkProgram(GLuint program);
+void APIENTRY openglCallbackFunction(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+                                       const GLchar* message, const void* userParam);
+void GetFirstNMessages(GLuint numMsgs);
+int CheckGLError(const char *file, int line);
+void enableDebugCallback();
+
+#if !defined(NDEBUG)
+#define CHECK_GL_ERROR()      CheckGLError(__FILE__, __LINE__);
+#define CHECK_GL_ERROR_MSG(X) printf(X); CheckGLError(__FILE__, __LINE__);
+#else
+#define CHECK_GL_ERROR()
+#define CHECK_GL_ERROR_MSG(X)
+#endif
